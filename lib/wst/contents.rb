a2a523c4d2beea15bca54e3668201bcf317ea748
Yves Brissaud <yves@sogilis.com>
2015-10-01 22:30:22 +0200

    Get pages/posts

--- lib/wst/contents.rb
+++ lib/wst/contents.rb
@@ -5,22 +5,36 @@ require 'wst/page'
 module Wst
   class Contents
     def initialize
-      @all = all_contents
+      @posts = Post.all
+      @pages = Page.all
+      @all = [@posts, @pages].flatten
     end
 
     # Get all contents
-    # @param [Boolean] show_non_publised Get all contents or only published
+    # @param [Boolean] show_non_published Get all contents or only published
     # @return [Array<Content>] Contents
     def all(show_non_published = false)
-      return @all if show_non_published
-      @all.select { |c| c.published }
+      get @all, show_non_published
+    end
+
+    # Get all pages
+    # @param [Boolean] show_non_published Get all pages or only published
+    # @return [Array<Content>] Contents
+    def pages(show_non_published = false)
+      get @pages, show_non_published
+    end
+
+    # Get all posts
+    # @param [Boolean] show_non_published Get all posts or only published
+    # @return [Array<Content>] Contents
+    def posts(show_non_published = false)
+      get @posts, show_non_published
     end
 
     private
-    # Get all contents, publised or not
-    # @return [Array<Content>] Contents
-    def all_contents
-      [Page.all, Post.all].flatten
+    def get(contents, show_non_published)
+      return contents if show_non_published
+      contents.select { |c| c.published }
     end
   end
 end
